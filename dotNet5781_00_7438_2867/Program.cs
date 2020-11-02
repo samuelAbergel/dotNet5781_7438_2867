2bf001fe7fae79b1d152c1c443ddd81153e60975
samuel abergel <samuelabergel123@gmail.com>
2020-11-02 14:21:59 +0200

    initial code + Welcome2867()

---
+++ dotNet5781_00_7438_2867/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_7438_2667
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome7438();
+            Welcome2867();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome2867();
+        private static void Welcome7438()
+        {
+            Console.WriteLine("Enter your name");
+            String userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
