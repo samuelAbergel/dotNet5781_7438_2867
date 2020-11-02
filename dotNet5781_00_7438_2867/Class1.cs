b4a03de82b822b8f104486c5bbff80ccfded901f
yechoua benchimol <yechouai@g.jct.ac.il>
2020-11-02 16:25:36 +0100

    welcome2867()

---
+++ dotNet5781_00_7438_2867/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_7438_2667
+{
+    partial class Program
+    {
+
+
+
+
+    }
+}
---
+++ dotNet5781_00_7438_2867/program2867.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_7438_2667
+{
+    partial class Program
+    {
+
+        static partial void Welcome2867()
+        {
+            Console.WriteLine("I am also here my friend");
+        }
+
+    }
+}
