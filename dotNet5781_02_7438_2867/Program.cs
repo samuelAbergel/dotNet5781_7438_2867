f68ff200071a9edd6ba4e6d8580ff3d2bd179db7
samuel abergel <samuelabergel123@gmail.com>
2020-11-11 15:13:35 +0200

    targuil 2

---
+++ dotNet5781_02_7438_2867/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_7438_2867
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
