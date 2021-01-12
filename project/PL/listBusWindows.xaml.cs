e5df25a066834bdc9dcf3661e832855a54f54148
samuel abergel <samuelabergel123@gmail.com>
2021-01-12 18:57:35 +0200

    tioud

--- project/PL/listBusWindows.xaml.cs
+++ project/PL/listBusWindows.xaml.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,7 +20,7 @@ using System.Windows.Shapes;
 namespace PL
 {
     /// <summary>
-    /// Logique d'interaction pour listBusWindows.xaml
+    /// windows of list of bus
     /// </summary>
     public partial class listBusWindows : Window
     {
@@ -30,91 +31,160 @@ namespace PL
         {
             bl = BLFactory.GetBL();
             InitializeComponent();
-            updateDataContext(); //fonction for update datacontext
+            updateDataContext();//call fonction to update the data context
 
         }
         void updateDataContext()
         {
-            collection = new ObservableCollection<BusPO>(from item in bl.GetAllBus() //get all bus from list of bus
+            collection = new ObservableCollection<BusPO>(from item in bl.GetAllBus()//get all buses from thelist
                                                          select new BusPO(item));
-            busList.DataContext = null; //and reset datacontext
-            busList.DataContext = collection;
+            busList.DataContext = null;
+            busList.DataContext = collection;//and reset the data context
         }
-
+        /// <summary>
+        /// button click to refuel a bus
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ButtonRefuelling_Click(object sender, RoutedEventArgs e)
         {
-            Button btn = sender as Button; //set bouton
-            busPO = btn.DataContext as BusPO;//set the bus to the datacontext(line of listview)
-            RefuellingWindows wnd = new RefuellingWindows(busPO.getBus());//open windows to do refuelling
-            wnd.ShowDialog();
-            updateDataContext();//update datacontext
+            Button btn = sender as Button;//set the button
+            busPO = btn.DataContext as BusPO;//set the line of listview to the bus
+            if (busPO.Status == BO.BusStatus.ReadyToGo)//if it's possible
+            {
+                RefuellingWindows wnd = new RefuellingWindows(busPO.getBus());//open the window to refuel
+                wnd.ShowDialog();
+                btn.IsEnabled = false;//set the button so that we cannot press
+                Refuelling(busPO, 100000, btn);//call fonction to use backgroundWOrker
+            }
         }
+
+        private void Refuelling(BusPO busPO, int time, Button btn)
+        {
+            List<object> lst = new List<object> { busPO, time, btn };//put all data in list
+            BackgroundWorker Refuelling = new BackgroundWorker();//create background worker
+            Refuelling.DoWork += Refuelling_DoWork;
+            Refuelling.RunWorkerCompleted += Refuelling_RunWorkedCompleted;
+            Refuelling.RunWorkerAsync(lst);
+        }
+
+        private void Refuelling_RunWorkedCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            List<Object> lst = (List<object>)e.Result;//take the list with all data
+            Button ButtonRefuelling_Click = lst[2] as Button;//set the button
+            BusPO bus = lst[0] as BusPO;//set the bus
+            bus.Status = BO.BusStatus.ReadyToGo; //set the bus
+:           ButtonRefuelling_Click.IsEnabled = true; //you can press the button
+            updateDataContext();//update the data context
+        }
+
+
+        private void Refuelling_DoWork(object sender, DoWorkEventArgs a)
+        {
+            List<object> lst = (List<object>)a.Argument;//set the list with all data
+            BusPO busPO = lst[0] as BusPO;//set the bus
+            busPO.Status = BO.BusStatus.refueling;//set the statue
+            int value = (int)lst[1];//take the value that you can't press during this period
+            Thread.Sleep(10000);//wait this period
+            a.Result = lst;//set the list to result
+        }
+
 
         private void ButtonTreatment_Click(object sender, RoutedEventArgs e)
         {
-            Button btn = sender as Button;//set button
-            busPO = btn.DataContext as BusPO;//set the bus to the datacontext(line of listview)
-            bl.treatment(busPO.getBus());//we call the function to treat the bus
-            updateDataContext();//we update the datacontext
+            Button btn = sender as Button;//set the button
+            busPO = btn.DataContext as BusPO;//set the bus
+            if (busPO.Status == BO.BusStatus.ReadyToGo)//if it's possible
+            {
+                bl.treatment(busPO.getBus());//use treatment of blimp
+                btn.IsEnabled = false;//you can't press the button
+                Treatment(busPO, 10000, btn);//call fonction with backgroun worker
+            }
         }
 
+
+        private void Treatment(BusPO busPO, int time, Button btn)
+        {
+            List<object> lst = new List<object> { busPO, time, btn };//set all data in list
+            BackgroundWorker Treatment = new BackgroundWorker();//create background worker
+            Treatment.DoWork += Treatment_DoWork;
+            Treatment.RunWorkerCompleted += Treatment_RunWorkedCompleted;
+            Treatment.RunWorkerAsync(lst);
+        }
+
+        private void Treatment_RunWorkedCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            List<Object> lst = (List<object>)e.Result;//set all data in list
+            Button ButtonTreatment_Click = lst[2] as Button;//set the button
+            BusPO bus = lst[0] as BusPO;//set the bus
+            bus.Status = BO.BusStatus.ReadyToGo;//set the statue
+            ButtonTreatment_Click.IsEnabled = true;//you can press the button
+            updateDataContext();//update the data context
+        }
+        private void Treatment_DoWork(object sender, DoWorkEventArgs a)
+        {
+            List<object> lst = (List<object>)a.Argument;//take all data inlist
+            BusPO busPO = lst[0] as BusPO;//set bus
+            busPO.Status = BO.BusStatus.inTreatment;//set statue
+            int value = (int)lst[1];//set value that you can't press the button during this period
+            Thread.Sleep(value);//wait this period
+            a.Result = lst;//set the list in result
+        }/// <summary>
+        /// when you double click you opzn information window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void busList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            BusPO bus = ((FrameworkElement)e.OriginalSource).DataContext as BusPO; //set bus as datacontext(line of listview)
-
-            if(bus != null)//if it exist
+            BusPO bus = ((FrameworkElement)e.OriginalSource).DataContext as BusPO;//set the bus
+            if (bus != null)//if it exist
             {
-                informationWindows wnd = new informationWindows(bus.getBus());//openwindow of information bus
+                informationWindows wnd = new informationWindows(bus.getBus());//open page withthe information
                 wnd.ShowDialog();
             }
         }
         /// <summary>
-        /// button click to return to home
+        /// to go to the home
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void ButtonHome_Click(object sender, RoutedEventArgs e)
         {
-            Opwindow wnd = new Opwindow();//open the home page
+            Opwindow wnd = new Opwindow();
             wnd.Show();
-            this.Close();//and close this page
+            this.Close();
         }
         /// <summary>
-        /// button click  to return a page before
+        /// to go to one page before
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void ButtonPreviousPage_Click(object sender, RoutedEventArgs e)
         {
-            MainBus wnd = new MainBus();//open a page before
+            MainBus wnd = new MainBus();
             wnd.Show();
-            this.Close();//and close this page
+            this.Close();
         }
-        /// <summary>
-        /// button for update the bus of this line
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
         {
-            Button btn = sender as Button;//set bus
-            busPO = btn.DataContext as BusPO;//set bus of line of listview
-            UpdateBus wnd = new UpdateBus(busPO.getBus());//open the page of update bus
+            Button btn = sender as Button;//set the button
+            busPO = btn.DataContext as BusPO;//set the bus
+            UpdateBus wnd = new UpdateBus(busPO.getBus());//open page for update
             wnd.ShowDialog();
-            updateDataContext();//and updat ethe data context
+            updateDataContext();//and update the datacontext
         }
         /// <summary>
-        /// button for remove the line
+        /// for remove a line of listview
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void ButtonRemove_Click(object sender, RoutedEventArgs e)
         {
-            Button btn = sender as Button;//set button to sender
-            busPO = btn.DataContext as BusPO;//set bus to line of listview
+            Button btn = sender as Button;//set the button
+            busPO = btn.DataContext as BusPO;//set the bus
             bl.removeBus(busPO.LicenseNum);//use remove from blimp
             updateDataContext();//and update the data context
         }
-
     }
+
 }
